ac35f06cb71b4e534d2dde3fdd70fe73ec6edf39
Anderson Lemos <bdl182@gmail.com>
2019-09-11 17:26:45 -0300

    update login page

---
+++ lib/pages/login.page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Color(0xFFF5F5F5),
+          padding: EdgeInsets.only(
+            top: 80,
+            left: 20,
+            right: 20,
+            bottom: 40,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 450,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black.withOpacity(0.12),
+                      offset: new Offset(5, 2.0),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: ListView(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                "Welcome",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text("Sign in to continue"),
+                            ],
+                          ),
+                          FlatButton(
+                              child: Text("Sign Up"), onPressed: () => {}),
+                        ],
+                      ),
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
